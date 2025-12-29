d442a80f824632ee51767ebbb83513158f4e2eef
Zephyr <willtien@outlook.com>
2025-12-29 11:15:36 +0800

    chore: change page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,9 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'M2VF - AI工作流编辑器',
+  title: 'M2VF - AI智能生成视频',
   description:
-    '一个强大的AI工作流编辑器，帮助您轻松创建和管理复杂的多模态工作流程',
+    '一款强大的AI工作流编排工具，帮助您轻松创建和管理复杂的多模态工作流程',
   icons: {
     icon: '/favicon.ico'
   }
